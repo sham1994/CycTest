67dd21e776901843107786f1cefa0fcaa4ad248a
Shamal De Saram <shamdzsar@gmail.com>
2018-10-22 10:04:35 +0530

    DB update Base_Entity

---
+++ CycTest/Definitions/Class1.cs
@@ -0,0 +1,12 @@
+﻿2using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Definitions
+{
+    public class Class1
+    {
+    }
+}
