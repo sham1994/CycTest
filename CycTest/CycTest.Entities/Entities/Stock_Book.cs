48072092d973654c7a440ad604bc88135e5dc032
Shamal De Saram <shamdzsar@gmail.com>
2018-10-22 09:39:10 +0530

    DB update

--- CycTest/CycTest.Entities/Entities/Stock_Book.cs
+++ CycTest/CycTest.Entities/Entities/Stock_Book.cs
@@ -1,10 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+
+
 namespace CycTest.Entities.Entities
 {
-    using System;
-    using System.Collections.Generic;
-    
+
     public partial class Stock_Book
     {
+        [Key] [Required] 
         public int ID { get; set; }
         public string Name { get; set; }
         public Nullable<int> Old_ID { get; set; }
